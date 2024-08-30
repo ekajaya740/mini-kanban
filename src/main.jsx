7e9db4f556cde02d6aa9261196d968f64cdfc194
ekajaya740 <ekajaya740@gmail.com>
2024-08-30 18:54:53 +0800

    feat: configure plugins and libraries, remove app.jsx and app.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './lib/config/reactQuery.js';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './lib/config/router.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
