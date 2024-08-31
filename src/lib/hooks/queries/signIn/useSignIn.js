a6d21d576bffd66a115baf349464124909e6ca3a
ekajaya740 <ekajaya740@gmail.com>
2024-08-31 11:11:16 +0800

    feat: add useSignIn hook

---
+++ src/lib/hooks/queries/signIn/useSignIn.js
@@ -0,0 +1,15 @@
+import { useQuery } from 'react-query';
+import Cookies from 'js-cookie';
+import { signInRequest } from '../../../requests/signIn/signInRequest';
+
+export function useSignIn() {
+  const queryKey = ['signin'];
+
+  return useQuery({
+    queryKey: queryKey,
+    queryFn: signInRequest,
+    onSuccess: async (data) => {
+      Cookies.set(import.meta.env.VITE_COOKIE_TOKEN_KEY, data.auth_token);
+    },
+  });
+}
