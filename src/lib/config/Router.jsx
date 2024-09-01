4539e9c62f5ce05e602776cb28d32dc15634bbba
ekajaya740 <ekajaya740@gmail.com>
2024-09-01 22:10:28 +0800

    feat: add page to router

--- src/lib/config/Router.jsx
+++ src/lib/config/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../../routes/RootLayout';
 import MainPage from '../../routes/v1/MainPage';
+import SignUpPage from '../../routes/v1/sign-up/SignUpPage';
+import SignInPage from '../../routes/v1/sign-in/SignInPage';
 
 export const router = createBrowserRouter(
   [
@@ -10,9 +12,17 @@ export const router = createBrowserRouter(
       children: [
         {
           index: true,
-          element: <MainPage />
-        }
-      ]
+          element: <MainPage />,
+        },
+      ],
+    },
+    {
+      path: '/sign-up',
+      element: <SignUpPage />,
+    },
+    {
+      path: '/sign-in',
+      element: <SignInPage />,
     },
   ],
   { basename: '/v1' }
