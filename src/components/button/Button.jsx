3812d4a95f188af1a498ac8717feb73c3de7bfd9
ekajaya740 <ekajaya740@gmail.com>
2024-09-01 22:05:49 +0800

    feat: add button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,15 +1,52 @@
+import clsx from 'clsx';
 import PropTypes from 'prop-types'; // ES6
 
 Button.propTypes = {
   children: PropTypes.any,
   onClick: PropTypes.func,
+  bordered: PropTypes.bool,
+  fullWidth: PropTypes.bool,
+  asLink: PropTypes.bool,
+  href: PropTypes.string,
+  color: PropTypes.string,
+  usePadding: PropTypes.bool,
 };
 
 export default function Button(props) {
-  const { children, onClick } = props;
+  const {
+    children,
+    onClick,
+    bordered,
+    fullWidth,
+    href,
+    asLink,
+    color,
+    usePadding,
+  } = props;
+
+  if (asLink) {
+    return (
+      <a
+        href={href}
+        className={clsx(
+          'underline w-full items-center justify-center inline-flex text-sm py-1',
+          {
+            'px-4': usePadding,
+          }
+        )}>
+        {children}
+      </a>
+    );
+  }
+
   return (
     <button
-      className='inline-flex space-x-2 text-sm items-center'
+      className={clsx('inline-flex space-x-2 text-sm items-center py-1', {
+        border: bordered,
+        'px-4': usePadding,
+        'w-full justify-center': fullWidth,
+        'bg-success text-white': color === 'success',
+      })}
       onClick={onClick}>
       {children}
     </button>
