532b600415da6273451aa663b8cb862aaa37e678
ekajaya740 <ekajaya740@gmail.com>
2024-09-01 17:03:27 +0800

    chore: merge staging with dev/components

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,11 @@ import { QueryClientProvider } from 'react-query';
 import { queryClient } from './lib/config/reactQuery.js';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './lib/config/router.jsx';
-import AntdConfig from './lib/config/antd.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AntdConfig>
-        <RouterProvider router={router} />
-      </AntdConfig>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
--- src/routes/v1/MainPage.jsx
+++ src/routes/v1/MainPage.jsx
@@ -1,7 +1,10 @@
+import TodoCard from '../../components/cards/TodoCard';
+
 export default function MainPage() {
   return (
     <main className='py-5 px-[3.125rem]'>
       <h1 className='text-xl font-bold'>Product Roadmap</h1>
+      <TodoCard />
     </main>
   );
 }
