86e762aea862977ac1298f938dfbacb2a620f896
ekajaya740 <ekajaya740@gmail.com>
2024-09-02 17:55:10 +0800

    fix: add noUnknown for schema conciseness

--- src/lib/schemas/auth.js
+++ src/lib/schemas/auth.js
@@ -2,21 +2,22 @@ import { object, string } from 'yup';
 import { baseDataSchema, baseResponseSchema } from './base';
 
 export const authSignUpRequestSchema = object({
-  name: string().required(),
   email: string().email().required(),
   password: string().required(),
-  password_confirmation: string().required(),
-});
-
-export const authSignUpResponseSchema = baseResponseSchema.shape({
-  data: baseDataSchema.concat(authSignInRequestSchema),
-});
+}).noUnknown();
 
 export const authSignInRequestSchema = authSignUpRequestSchema.omit([
-  'name',
   'password_confirmation',
 ]);
 
-export const authSignInResponseSchema = baseResponseSchema.shape({
-  token: string(),
-});
+export const authSignUpResponseSchema = baseResponseSchema
+  .shape({
+    data: baseDataSchema.concat(authSignInRequestSchema),
+  })
+  .noUnknown();
+
+export const authSignInResponseSchema = baseResponseSchema
+  .shape({
+    token: string(),
+  })
+  .noUnknown();
