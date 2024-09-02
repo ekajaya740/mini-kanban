9f2f59e827c59d29fe174bbda674d89d97a639f0
ekajaya740 <ekajaya740@gmail.com>
2024-09-02 17:55:43 +0800

    feat: implement the protected routes

--- src/lib/config/Router.jsx
+++ src/lib/config/Router.jsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Layout from '../../routes/RootLayout';
 import MainPage from '../../routes/v1/MainPage';
-import SignUpPage from '../../routes/v1/sign-up/SignUpPage';
-import SignInPage from '../../routes/v1/sign-in/SignInPage';
+import SignUpPage from '../../routes/v1/auth/sign-up/SignUpPage';
+import SignInPage from '../../routes/v1/auth/sign-in/SignInPage';
+import ProtectedMainLayout from '../../routes/v1/ProtectedMainLayout';
+import ProtectedAuthLayout from '../../routes/v1/auth/ProtectedAuthLayout';
 
 export const router = createBrowserRouter(
   [
     {
       path: '/',
-      element: <Layout />,
+      element: <ProtectedMainLayout />,
       children: [
         {
           index: true,
@@ -17,12 +18,20 @@ export const router = createBrowserRouter(
       ],
     },
     {
-      path: '/sign-up',
-      element: <SignUpPage />,
-    },
-    {
-      path: '/sign-in',
-      element: <SignInPage />,
+      path: '/auth',
+      element: <ProtectedAuthLayout />,
+      children: [
+        {
+          index: true,
+          path: 'sign-up',
+          element: <SignUpPage />,
+        },
+        {
+          index: true,
+          path: 'sign-in',
+          element: <SignInPage />,
+        },
+      ],
     },
   ],
   { basename: '/v1' }
