d4bf37e3245654dc9ada29ab4ab0a322e338dd02
ekajaya740 <ekajaya740@gmail.com>
2024-09-02 17:53:50 +0800

    fix: change name to email

--- src/components/forms/SignInForm.jsx
+++ src/components/forms/SignInForm.jsx
@@ -5,7 +5,7 @@ export default function SignInForm() {
   const { control } = useFormContext();
   return (
     <form className='space-y-2'>
-      <Input label={'Nama'} control={control} name={'name'} />
+      <Input label={'Email'} control={control} name={'email'} />
       <Input label={'Password'} control={control} name={'password'} />
     </form>
   );
