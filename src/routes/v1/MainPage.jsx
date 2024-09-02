e2c1f78663c7fd8be1aa494c2c1e265300a9a791
ekajaya740 <ekajaya740@gmail.com>
2024-09-02 18:41:24 +0800

    fix: change todo card to todos list

--- src/routes/v1/MainPage.jsx
+++ src/routes/v1/MainPage.jsx
@@ -1,10 +1,10 @@
-import TodoCard from '../../components/cards/TodoCard';
+import TodosList from '../../components/lists/TodosList';
 
 export default function MainPage() {
   return (
-    <main className='py-5 px-[3.125rem] space-y-4'>
+    <main className='py-5 px-[3.125rem] space-y-4 w-full'>
       <h1 className='text-xl font-bold'>Product Roadmap</h1>
-      <TodoCard />
+      <TodosList />
     </main>
   );
 }
