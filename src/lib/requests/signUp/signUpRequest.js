103377261750d4706a87f1711b6a756b919b3e2c
ekajaya740 <ekajaya740@gmail.com>
2024-09-02 17:54:33 +0800

    chore: remove console.log

--- src/lib/requests/signUp/signUpRequest.js
+++ src/lib/requests/signUp/signUpRequest.js
@@ -2,10 +2,11 @@ import { axiosInstance } from '../../config/axios';
 import { authSignUpResponseSchema } from '../../schemas/auth';
 
 export async function signUpRequest(input) {
-  const endpoint = '/sign-up';
+  const endpoint = '/auth/sign-up';
 
   const { data } = await axiosInstance.post(endpoint, input);
 
+  console.log('data', data);
   const vData = await authSignUpResponseSchema.validate(data);
 
   return vData;
