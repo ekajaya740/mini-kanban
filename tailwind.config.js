d2382db4315438a74121c45335ddcb3731644a56
ekajaya740 <ekajaya740@gmail.com>
2024-09-03 21:00:29 +0800

    fix: add some color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,46 +9,57 @@ export default {
         magenta: {
           border: '#FFADD2',
           bg: '#FFF0F6',
+          text: '#EB2F96',
         },
         red: {
           border: '#FFA39E',
           bg: '#FFF1F0',
+          text: '#F5222D',
         },
         volcano: {
-          bg: '#FFBB96',
+          bg: '#FFF2E8',
           border: '#FFBB96',
+          text: '#FA541C',
         },
         orange: {
           bg: '#FFF7E6',
           border: '#FFD591',
+          text: '#FA8C16',
         },
         gold: {
           bg: '#FFFBE6',
           border: '#FFE58F',
+          text: '#FAAD14',
         },
         lime: {
           bg: '#FCFFE6',
           border: '#EAFF8F',
+          text: '#A0D911',
         },
         green: {
           bg: '#F6FFED',
           border: '#B7EB8F',
+          text: '#52C41A',
         },
         cyan: {
           bg: '#E6FFFB',
           border: '#87E8DE',
+          text: '#13C2C2',
         },
         blue: {
           bg: '#E6F7FF',
           border: '#91D5FF',
+          text: '#1890FF',
         },
         geekblue: {
           bg: '#F0F5FF',
           border: '#ADC6FF',
+          text: '#2F54EB',
         },
         purple: {
           bg: '#F9F0FF',
           border: '#D3ADF7',
+          text: '#722ED1',
         },
       },
     },
