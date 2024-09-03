a55532e3519b5bba20fb509e61adc7d0fe3e1091
ekajaya740 <ekajaya740@gmail.com>
2024-09-03 17:51:34 +0800

    fix: resolve merge with TodoItemsList

--- src/components/lists/TodoItemsList.jsx
+++ src/components/lists/TodoItemsList.jsx
@@ -9,19 +9,12 @@ TodoItemsList.propTypes = {
 export default function TodoItemsList(props) {
   const { task } = props;
   return (
-<<<<<<< Updated upstream
-    <div className='space-y-3'>
-      <TodoItemCard />
-      <TodoItemCard />
-      <TodoItemCard />
-=======
     <div className='space-y-3 py-3'>
       {task.length === 0 ? (
         <p>Tidak ada data</p>
       ) : (
         task.map((item) => <TaskCard key={item.id} index={item.id} {...item} />)
       )}
->>>>>>> Stashed changes
     </div>
   );
 }
